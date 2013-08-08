db421d2dc8f7f9a8267ed6a6eaef168fcf09c460
Stu Kabakoff <stuart.kabakoff@mlb.com>
2013-08-08 16:47:09 -0400

    initial commit

---
+++ amd.js
@@ -0,0 +1,5 @@
+define(['text!./test.txt'], function(sampleText) {
+//define(function(require) {
+//	var sampleText = require('./text.txt');
+	console.log(sampleText);
+});
---
+++ test.js
@@ -0,0 +1,5 @@
+define(['text!./test.txt', './dependency'], function(sampleText) {
+//define(function(require) {
+//	var sampleText = require('text!./text.txt');
+	console.log(sampleText);
+});
